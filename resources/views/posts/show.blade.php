b1f5f923628b95b34dc95ab2fa6838150b554560
Alicja Krystofiak <AlicjaKr@realbud.dyndns.biz>
2020-02-17 13:55:58 +0100

    suwanie postow

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -10,4 +10,9 @@
     </div>
     <hr>
     <a href = "/posts/{{$post->id}}/edit" clas="btn btn-default">Edit Post</a>
+
+    {!!Form::open(['axtion' => ['PostController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
+        {{Form::hidden('_method', 'DELETE')}}
+        {{Form::submit('Delete', ['class' =>'btn btn-danger'])}}
+    {!!Form::close()!!}
 @endsection
